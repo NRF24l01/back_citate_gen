7e2fbb0321bde6b5b0702dff46b3565e37c4d783
nrf24l01 <s.nevrovsky@ya.ru>
2025-04-16 22:46:09 +0300

    Fixed uuid

--- models/base.go
+++ models/base.go
@@ -9,7 +9,7 @@ import (
 
 
 type BaseModel struct {
-	ID        uuid.UUID      `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()" json:"id"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid()" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
