a8943c88621f5bf7375c7cf5a96a51092b8977ff
nrf24l01 <s.nevrovsky@ya.ru>
2025-04-17 23:23:10 +0300

    Made username unique

--- models/user.go
+++ models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	BaseModel
 	Email string `json:"email" gorm:"uniqueIndex"`
 	Password string `json:"password"`
-	Username string `json:"username"`
+	Username string `json:"username" gorm:"uniqueIndex"`
 }
